a7b2cf31096009654872f8cb6cd8ed6856784a1c
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:21:25 +0530

    Add files via upload

---
+++ Evaluate.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,d,e,f,g,h,k;
+	a=8,b=2,c=8,d=4,e=8,f=9;
+	printf("%d\n",a+b-(c-d)*3%e+f/9);
+		a=8,b=2,c=8,d=4,e=8,f=9;
+	printf("%d\n",a%6-b/2+(c*d-5)/e);
+	a=8,b=2,c=8,d=4,e=8,f=9;
+	printf("%d\n",a*b-c/d<e+f);
+	return 0;
+}
