72e927001d5a7882404d0c282e3d34c190163e75
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:30:02 +0530

    Add files via upload

---
+++ Opretor.cpp
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int a=17,b=14;
+	printf("Add %d\n",a+b);
+	printf("Sub %d\n",a-b);
+	printf("mul %d\n",a*b);
+	printf("div %d\n",a/b);
+	return 0;
+}
