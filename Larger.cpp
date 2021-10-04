5857b69f16def88a71449aabf03d3a64393d56e2
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:24:46 +0530

    Add files via upload

---
+++ Larger.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=10,b=20,MAX;
+	printf("Show thise larger value\n");
+	MAX=a>b?a:b;
+	printf("Larger Value%d\t %d\t is %d\n",a,b,MAX);
+	return 0;
+}
