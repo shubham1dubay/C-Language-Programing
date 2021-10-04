b6617f5f7644c8b1a1e774d5f1f8c58a0efdf05c
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:18:31 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int length,breath,area;
+	//length=10;
+	//breath=5;
+	printf("Enter the area\n");
+	scanf("%d %d",&length,&breath);
+	area=length*breath;
+	printf("%d",area);
+	return 0;
+}
