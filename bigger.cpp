de5edb1e2cafcb860f3464d4fc35bfcd753700d2
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:18:48 +0530

    Add files via upload

---
+++ bigger.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,big;
+	printf("Enter the number used\n");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		big=a;
+		else n
+		big=c;
+	}
+	else 
+	{
+		if(b>c)
+		big=b;
+		else 
+		big=c;
+	}
+	printf("Bogger number is %d\n",big);
+	return 0;
+}
