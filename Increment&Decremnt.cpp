fe58e604b21c706f0b8d51cedc1eb8ca7e92a2f0
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:24:21 +0530

    Add files via upload

---
+++ Increment&Decremnt.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	int a=8;
+	printf("A= %d\n",a);
+	printf("A= %d\n",a++);
+	printf("A= %d\n",++a);
+	printf("A= %d\n",a--);
+	printf("A= %d\n",--a);
+	return 0;
+}
