17b03b1ad3b3c20a6fc376017ddf622c03042606
Shubham Dubey <88899447+shubham1dubay@users.noreply.github.com>
2021-10-04 11:32:14 +0530

    Add files via upload

---
+++ Size.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main()
+{
+	printf("sozeof(char) %u\n",sizeof(char));
+	printf("sozeof(int) %u\n",sizeof(int));
+	printf("sozeof(float) %u\n",sizeof(float));
+	printf("sozeof(char) %u\n",sizeof(char));
+	printf("sozeof(double) %u\n",sizeof(double));
+}
